f0e45a2ac1c1a5ea129a3e9468a511ffb0223f17
anshanxulizhi <15001295170@163.com>
2018-11-09 13:26:11 +0800

    用户Controller

---
+++ src/main/java/com/xulizhi/demo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.xulizhi.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author lenovo
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+
+}
