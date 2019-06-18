9aa8694287474f5d468379ad65c762527a3c6190
lenovo <15001295170@163.com>
2019-06-18 15:50:29 +0800

    注解式Redis缓存

--- src/main/java/com/xulizhi/demo/SpringbootDemoApplication.java
+++ src/main/java/com/xulizhi/demo/SpringbootDemoApplication.java
@@ -2,10 +2,12 @@ package com.xulizhi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author 10133
  */
+@EnableCaching
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
--- src/main/java/com/xulizhi/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/xulizhi/demo/service/impl/UserServiceImpl.java
@@ -5,21 +5,26 @@ import com.xulizhi.demo.domain.User;
 import com.xulizhi.demo.dto.UserDTO;
 import com.xulizhi.demo.repository.UserRepository;
 import com.xulizhi.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
-public class UserServoceImpl implements UserService {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Cacheable(value = "UserInfo",keyGenerator = "simpleKeyGenerator") // 3000秒
     @Override
     public List<UserDTO> queryUserList() {
 
+        log.info("查询数据库-----------------------------------------------------------------");
         List<UserDTO> userDTOList = new ArrayList<UserDTO>();
         List<User> userList = userRepository.findByDeleted(DeletedStatusEnum.NOTDEL.getCode());
         for(int i=0;i<userList.size();i++){
@@ -34,8 +39,10 @@ public class UserServoceImpl implements UserService {
         return userDTOList;
     }
 
+    @Cacheable(value = "UserInfo",keyGenerator = "simpleKeyGenerator") // 3000秒
     @Override
     public UserDTO queryUserById(String userId) {
+        log.info("查询数据库-----------------------------------------------------------------");
         UserDTO userDTO = new UserDTO();
         User user = userRepository.getOne(userId);
         userDTO.setId(user.getId());
